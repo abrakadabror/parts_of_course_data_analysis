99bb1f613f5721a39f5e90cf51e53a1f0f949760
Kamil <77681807+abrakadabror@users.noreply.github.com>
2023-07-24 08:19:48 +0200

    #tworzenie rozpakowanej listy. Rozumiane przez to, ze lista item_list zawiera #dokladnie trzy elementy i sa one przypisane do trzech wartosci s,b,h.

--- list_indexing.py
+++ list_indexing.py
@@ -16,3 +16,17 @@ print(item_list[3].lstrip('G')) #poda nam 4 element z listy liczac od 0 jako sno
 #zamieniamy 'Boots' na 'Jeans'
 modyfied_list = [item.replace('Boots', 'Jeans')for item in item_list] 
 print(modyfied_list)
+
+#drukujemy jedynie 3 pierwze elementy z listy
+item_list = ['Snowboard', 'Boots','Helmet', 'Googles', 'Bindings']
+print(item_list[:3]) 
+
+print(item_list[::2]) #pobiera co drugu element z listy
+print(item_list[2:4]) #pobiera 3 element o indexie 2(statrtujemy od zera) 
+#pobiera tez 4 element o indexie 3(rowniez statrujemy od zera)
+
+#tworzenie rozpakowanej listy. Rozumiane przez to, ze lista item_list zawiera
+#dokladnie trzy elementy i sa one przypisane do trzech wartosci s,b,h.
+item_list = ['Snowboard', 'Boots', 'Helmet']
+s, b ,h = item_list
+print(s, b, h)
