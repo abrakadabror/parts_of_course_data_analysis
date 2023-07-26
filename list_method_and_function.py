2ca79daee2082115bd7bc40db97c25483520b8d1
Kamil <77681807+abrakadabror@users.noreply.github.com>
2023-07-26 22:12:07 +0200

    list methods

---
+++ list_method_and_function.py
@@ -0,0 +1,10 @@
+transaction = [10.44, 20.56, 200.14, 1242.66, 2.07, 8.01]
+
+print(len(transaction)) #podaje ilosc elementow w liscie
+print(sum(transaction)) #podaje sume
+print(min(transaction)) #podaje minimalna 
+print(max(transaction)) #podaje max wartosc z listy
+      
+print(sorted(transaction)) #sortuje bez przestawiania danych w oryginalnej liscie
+
+print(transaction.index(20.56))  #zwraca info ktory to index w liscie
