b86f3fc700edc8e56b4def6143c45641a434aa55
Kamil <77681807+abrakadabror@users.noreply.github.com>
2023-08-01 12:28:43 +0200

    #Looping Over Indices & Multiple Iterables

--- new_assigments_for_loops.py
+++ new_assigments_for_loops.py
@@ -29,3 +29,20 @@ print(empty_list_rating) #drukujemy wyniki
 
 print(round(sum(empty_list_rating) /len(empty_list_rating),1)) #obliczmay srednia i zaaokrogalmy do pierwszego miejsca po przecinku
 
+#Looping Over Indices & Multiple Iterables
+
+exchange_rate = 0.88
+usd_list = [5.99, 9.99, 19.99, 23.99, 34.99 ]
+
+euro_list = [] #tworzymy pusta liste
+
+for i in range(len(usd_list)):
+    euro_list.append(round(usd_list[i] * exchange_rate, 2)) #dodajemy do pustej listy przeliczone elementy z usd_list * exchaneg_rate
+print(euro_list) #drukujemy wynik
+ 
+
+euro_list = [5.29, 9.19, 19.69, 23.39, 34.29]
+item_list = ['Snowboard', 'Boots', 'Helmet', 'Googles', "Bindings"]
+
+for i in range(len(euro_list)): #w zakresie ilosci elementow znajdujacyh sie w liscie euro_list drukujemy:
+    print(f'The {item_list[i].lower()} costs ${euro_list[i]}') 
